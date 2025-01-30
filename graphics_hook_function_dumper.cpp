d6c49d5e818020910548f258d1cb2cc87e96d378
Connor J Thompson <ct770a@gmail.com>
2025-01-31 02:16:23 +1100

    setup an incredibly primitive & manual way of finding the function ptrs, we could possibly assume that the vtable index of the function will always be the same though, so we can potentially automate after digging up the offsets for each function

--- graphics_hook_function_dumper.cpp
+++ graphics_hook_function_dumper.cpp
@@ -1,11 +1,190 @@
-// graphics_hook_function_dumper.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <iostream>
+#include <string>
 
 #include <iostream>
+#include <fstream>
+
+#include <windows.h>
+#include <d3d11_1.h>
+#pragma comment(lib, "d3d11.lib")
+#include <d3dcompiler.h>
+#pragma comment(lib, "d3dcompiler.lib")
+#include <intrin.h>
+
+unsigned long long calculateChecksum(const std::string& filename) {
+    std::ifstream file(filename, std::ios::binary);
+    if (!file.is_open()) {
+        std::cerr << "Unable to open file: " << filename << std::endl;
+        return 0;}
+    unsigned long long checksum = 0;
+    char ch;
+    while (file.get(ch))
+        checksum += static_cast<unsigned char>(ch);
+    return checksum;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam){
+    switch (msg){
+    case WM_KEYDOWN:
+    case WM_KEYUP:
+    case WM_SIZE:
+        return 0;
+    case WM_DESTROY:{
+        PostQuitMessage(0);
+        return 0;}
+    default:
+        return DefWindowProcW(hwnd, msg, wparam, lparam);
+    }
+}
+
+void generate_dx11_junk(ID3D11Device1*& d3d11Device, ID3D11DeviceContext1*& d3d11DeviceContext, IDXGISwapChain1*& d3d11SwapChain) {
+    // Open a window
+    HWND hwnd;
+    {
+        WNDCLASSEXW winClass = {};
+        winClass.cbSize = sizeof(WNDCLASSEXW);
+        winClass.style = CS_HREDRAW | CS_VREDRAW;
+        winClass.lpfnWndProc = &WndProc;
+        winClass.hInstance = GetModuleHandleW(0);
+        winClass.hIcon = LoadIconW(0, IDI_APPLICATION);
+        winClass.hCursor = LoadCursorW(0, IDC_ARROW);
+        winClass.lpszClassName = L"MyWindowClass";
+        winClass.hIconSm = LoadIconW(0, IDI_APPLICATION);
+
+        if (!RegisterClassExW(&winClass)) {
+            MessageBoxA(0, "RegisterClassEx failed", "Fatal Error", MB_OK);
+            return; // GetLastError();
+        }
+
+        RECT initialRect = { 0, 0, 1024, 768 };
+        AdjustWindowRectEx(&initialRect, WS_OVERLAPPEDWINDOW, FALSE, WS_EX_OVERLAPPEDWINDOW);
+        LONG initialWidth = initialRect.right - initialRect.left;
+        LONG initialHeight = initialRect.bottom - initialRect.top;
+
+        hwnd = CreateWindowExW(WS_EX_OVERLAPPEDWINDOW,
+            winClass.lpszClassName,
+            L"08. Drawing a Cube",
+            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+            CW_USEDEFAULT, CW_USEDEFAULT,
+            initialWidth,
+            initialHeight,
+            0, 0, winClass.hInstance, 0);
+
+        if (!hwnd) {
+            MessageBoxA(0, "CreateWindowEx failed", "Fatal Error", MB_OK);
+            return; // GetLastError();
+        }
+    }
+
+    // Create D3D11 Device and Context
+    {
+        ID3D11Device* baseDevice;
+        ID3D11DeviceContext* baseDeviceContext;
+        D3D_FEATURE_LEVEL featureLevels[] = { D3D_FEATURE_LEVEL_11_0 };
+        UINT creationFlags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
+
+        HRESULT hResult = D3D11CreateDevice(0, D3D_DRIVER_TYPE_HARDWARE,
+            0, creationFlags,
+            featureLevels, ARRAYSIZE(featureLevels),
+            D3D11_SDK_VERSION, &baseDevice,
+            0, &baseDeviceContext);
+        if (FAILED(hResult)) {
+            MessageBoxA(0, "D3D11CreateDevice() failed", "Fatal Error", MB_OK);
+            return; // GetLastError();
+        }
+
+        // Get 1.1 interface of D3D11 Device and Context
+        hResult = baseDevice->QueryInterface(__uuidof(ID3D11Device1), (void**)&d3d11Device);
+        //assert(SUCCEEDED(hResult));
+        baseDevice->Release();
+
+        hResult = baseDeviceContext->QueryInterface(__uuidof(ID3D11DeviceContext1), (void**)&d3d11DeviceContext);
+        //assert(SUCCEEDED(hResult));
+        baseDeviceContext->Release();
+    }
+
+    // Create Swap Chain
+    {
+        // Get DXGI Factory (needed to create Swap Chain)
+        IDXGIFactory2* dxgiFactory;
+        {
+            IDXGIDevice1* dxgiDevice;
+            HRESULT hResult = d3d11Device->QueryInterface(__uuidof(IDXGIDevice1), (void**)&dxgiDevice);
+            //assert(SUCCEEDED(hResult));
+
+            IDXGIAdapter* dxgiAdapter;
+            hResult = dxgiDevice->GetAdapter(&dxgiAdapter);
+            //assert(SUCCEEDED(hResult));
+            dxgiDevice->Release();
+
+            DXGI_ADAPTER_DESC adapterDesc;
+            dxgiAdapter->GetDesc(&adapterDesc);
+
+            OutputDebugStringA("Graphics Device: ");
+            OutputDebugStringW(adapterDesc.Description);
+
+            hResult = dxgiAdapter->GetParent(__uuidof(IDXGIFactory2), (void**)&dxgiFactory);
+            //assert(SUCCEEDED(hResult));
+            dxgiAdapter->Release();
+        }
+
+        DXGI_SWAP_CHAIN_DESC1 d3d11SwapChainDesc = {};
+        d3d11SwapChainDesc.Width = 0; // use window width
+        d3d11SwapChainDesc.Height = 0; // use window height
+        d3d11SwapChainDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM_SRGB;
+        d3d11SwapChainDesc.SampleDesc.Count = 1;
+        d3d11SwapChainDesc.SampleDesc.Quality = 0;
+        d3d11SwapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+        d3d11SwapChainDesc.BufferCount = 2;
+        d3d11SwapChainDesc.Scaling = DXGI_SCALING_STRETCH;
+        d3d11SwapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+        d3d11SwapChainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+        d3d11SwapChainDesc.Flags = 0;
+
+        HRESULT hResult = dxgiFactory->CreateSwapChainForHwnd(d3d11Device, hwnd, &d3d11SwapChainDesc, 0, 0, &d3d11SwapChain);
+        //assert(SUCCEEDED(hResult));
+
+        dxgiFactory->Release();
+    }
+
+}
+
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+    // iterate all running windows processes
+
+    // check loaded modules for either DirectX, vulkan, or whatever
+    std::string target_dll = "C:\\Windows\\System32\\d3d11.dll";
+    unsigned long long checksum = calculateChecksum(target_dll);
+    std::cout << "Checksum: " << checksum << std::endl;
+    
+
+    ID3D11Device1* d3d11Device;
+    ID3D11DeviceContext1* d3d11DeviceContext;
+    IDXGISwapChain1* d3d11SwapChain;
+    generate_dx11_junk(d3d11Device, d3d11DeviceContext, d3d11SwapChain);
+
+    // turns out C++ doesn't let you get the pointer to virtual functions??? thanks bill gates
+    // so we just have to do this the hard way
+    //__asm {int 3; }
+    //asm("int3");
+
+    std::string test;
+    std::cin >> test;
+
+    __debugbreak();
+    d3d11DeviceContext->DrawIndexed(0, 0, 0);
+    __debugbreak();
+    d3d11DeviceContext->VSSetShader(nullptr, nullptr, 0);
+    __debugbreak();
+    d3d11DeviceContext->VSSetConstantBuffers(0, 1, nullptr);
+    __debugbreak();
+    d3d11SwapChain->Present(1, 0);
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
